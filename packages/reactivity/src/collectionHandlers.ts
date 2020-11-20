65ea709dac46e4310eb2ac95cb19984d9b921d88
gcclll <gccll.love@gmail.com>
2020-11-20 15:55:00 +0800

    feat: add get proxy handler

--- packages/reactivity/src/collectionHandlers.ts
+++ packages/reactivity/src/collectionHandlers.ts
@@ -1,24 +1,10 @@
-import { hasOwn, isObject } from '@vue/shared'
-import { TrackOpTypes } from './operations'
-import { reactive, ReactiveFlags, readonly, toRaw } from './reactive'
-import { track } from './effect'
+import { hasOwn } from '@vue/shared'
+import { ReactiveFlags } from './reactive'
 
 export type CollectionTypes = IterableCollections | WeakCollections
 type IterableCollections = Map<any, any> | Set<any>
 type WeakCollections = WeakMap<any, any> | WeakSet<any>
 type MapTypes = Map<any, any> | WeakMap<any, any>
-type SetTypes = Set<any> | WeakSet<any>
-
-const toReactive = <T extends unknown>(value: T): T =>
-  isObject(value) ? reactive(value) : value
-
-const toReadonly = <T extends unknown>(value: T): T =>
-  isObject(value) ? readonly(value as Record<any, any>) : value
-
-const toShallow = <T extends unknown>(value: T): T => value
-
-const getProto = <T extends CollectionTypes>(v: T): any =>
-  Reflect.getPrototypeOf(v)
 
 function get(
   target: MapTypes,
