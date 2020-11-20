a5e8e062658d458081ce1bb499b8041f6175689e
gcclll <gccll.love@gmail.com>
2020-11-20 15:31:23 +0800

    feat(add): collection get proxy

--- packages/reactivity/src/collectionHandlers.ts
+++ packages/reactivity/src/collectionHandlers.ts
@@ -1,5 +1,5 @@
-import { isObject } from '@vue/shared'
-import { reactive, readonly } from './reactive'
+import { hasOwn, isObject } from '@vue/shared'
+import { reactive, ReactiveFlags, readonly } from './reactive'
 
 export type CollectionTypes = IterableCollections | WeakCollections
 type IterableCollections = Map<any, any> | Set<any>
@@ -18,9 +18,40 @@ const toShallow = <T extends unknown>(value: T): T => value
 const getProto = <T extends CollectionTypes>(v: T): any =>
   Reflect.getPrototypeOf(v)
 
+const mutableInstrumentations: Record<string, Function> = {
+  get(this: MapTypes, key: unknown) {
+    return get(this, key)
+  },
+  get size() {
+    return size((this as unknown) as IterableCollections)
+  }
+}
+
 function createInstrumentationGetter(isReadonly: boolean, shallow: boolean) {
-  // TODO
+  const instrumentations = mutableInstrumentations
+
+  return (
+    target: CollectionTypes,
+    key: string | symbol,
+    receiver: CollectionTypes
+  ) => {
+    if (key === ReactiveFlags.IS_REACTIVE) {
+      return !isReadonly
+    } else if (key === ReactiveFlags.IS_READONLY) {
+      return isReadonly
+    } else if (key === ReactiveFlags.RAW) {
+      return target
+    }
+
+    return Reflect.get(
+      hasOwn(instrumentations, key) && key in target
+        ? instrumentations
+        : target,
+      key,
+      receiver
+    )
+  }
 }
 export const mutableCollectionHandlers: ProxyHandler<CollectionTypes> = {
-  // get: createInstrumentationGetter(false, false)
+  get: createInstrumentationGetter(false, false)
 }
