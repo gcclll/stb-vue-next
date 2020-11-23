315e0d91869dd4332c740e56d4e385f04b6009e9
gcclll <gccll.love@gmail.com>
2020-11-23 20:52:20 +0800

    feat(add): computed function reloads

--- packages/reactivity/src/computed.ts
+++ packages/reactivity/src/computed.ts
@@ -1,11 +1,27 @@
+import { ReactiveEffect } from './effect'
 import { Ref } from './ref'
 
-export interface ComputedRef<T = any> extends WritableComputedRef<T> {}
+export interface ComputedRef<T = any> extends WritableComputedRef<T> {
+  readonly value: T
+}
 
-export interface WritableComputedRef<T> extends Ref<T> {}
+export interface WritableComputedRef<T> extends Ref<T> {
+  readonly effect: ReactiveEffect<T>
+}
 
 export type ComputedGetter<T> = (ctx?: any) => T
 
 export type ComputedSetter<T> = (v: T) => void
 
-export interface WritableComputedOptions<T> {}
+export interface WritableComputedOptions<T> {
+  get: ComputedGetter<T>
+  set: ComputedSetter<T>
+}
+
+export function computed<T>(getter: ComputedGetter<T>): ComputedRef<T>
+export function computed<T>(
+  options: WritableComputedOptions<T>
+): WritableComputedRef<T>
+export function computed<T>(
+  getterOrOptions: ComputedGetter<T> | WritableComputedOptions<T>
+) {}
