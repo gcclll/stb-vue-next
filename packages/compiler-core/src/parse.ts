a86565029f35fda94d72a654ff49fcb6d28cf775
gcclll <gccll.love@gmail.com>
2020-11-24 15:30:13 +0800

    feat(init): cc type definitions

---
+++ packages/compiler-core/src/parse.ts
@@ -0,0 +1,8 @@
+export const enum TextModes {
+  //          | Elements | Entities | End sign              | Inside of
+  DATA, //    | ✔        | ✔        | End tags of ancestors |
+  RCDATA, //  | ✘        | ✔        | End tag of the parent | <textarea>
+  RAWTEXT, // | ✘        | ✘        | End tag of the parent | <style>,<script>
+  CDATA,
+  ATTRIBUTE_VALUE
+}
--- packages/compiler-core/src/transform.ts
+++ packages/compiler-core/src/transform.ts
@@ -1,6 +1,53 @@
-import { ExpressionNode } from './ast'
+import {
+  ExpressionNode,
+  RootNode,
+  TemplateChildNode,
+  JSChildNode,
+  SimpleExpressionNode,
+  CacheExpression
+} from './ast'
+
+// There are two types of transforms:
+//
+// - NodeTransform:
+//   Transforms that operate directly on a ChildNode. NodeTransforms may mutate,
+//   replace or remove the node being processed.
+export type NodeTransform = (
+  node: RootNode | TemplateChildNode,
+  context: TransformContext
+) => void | (() => void) | (() => void)[]
 
 export interface ImportItem {
   exp: string | ExpressionNode
   path: string
+}
+
+export interface TransformContext extends Required<TransformOptions> {
+  root: RootNode
+  helpers: Set<symbol>
+  components: Set<string>
+  directives: Set<string>
+  hoists: (JSChildNode | null)[]
+  imports: Set<ImportItem>
+  temps: number
+  cached: number
+  identifiers: { [name: string]: number | undefined }
+  scopes: {
+    vFor: number
+    vSlot: number
+    vPre: number
+    vOnce: number
+  }
+  parent: ParentNode | null
+  childIndex: number
+  currentNode: RootNode | TemplateChildNode | null
+  helper<T extends symbol>(name: T): T
+  helperString(name: symbol): string
+  replaceNode(node: TemplateChildNode): void
+  removeNode(node?: TemplateChildNode): void
+  onNodeRemoved(): void
+  addIdentifiers(exp: ExpressionNode | string): void
+  removeIdentifiers(exp: ExpressionNode | string): void
+  hoist(exp: JSChildNode): SimpleExpressionNode
+  cache<T extends JSChildNode>(exp: T, isVNode?: boolean): CacheExpression | T
 }
