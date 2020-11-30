e03a03c5d775ff9315cc027d88b0669a775cf590
gcclll <gccll.love@gmail.com>
2020-11-30 10:22:33 +0800

    feat(init): transform section

--- packages/compiler-core/src/transform.ts
+++ packages/compiler-core/src/transform.ts
@@ -1,11 +1,17 @@
+import { NOOP } from '@vue/shared'
 import {
   ExpressionNode,
   RootNode,
   TemplateChildNode,
   JSChildNode,
   SimpleExpressionNode,
-  CacheExpression
+  CacheExpression,
+  ElementNode,
+  DirectiveNode,
+  ParentNode
 } from './ast'
+import { defaultOnError } from './errors'
+import { TransformOptions } from './options'
 
 // There are two types of transforms:
 //
@@ -21,6 +27,14 @@ export interface ImportItem {
   exp: string | ExpressionNode
   path: string
 }
+
+// A structural directive transform is a technically a NodeTransform;
+// Only v-if and v-for fall into this category.
+export type StructuralDirectiveTransform = (
+  node: ElementNode,
+  dir: DirectiveNode,
+  context: TransformContext
+) => void | (() => void)
 
 export interface TransformContext extends Required<TransformOptions> {
   root: RootNode
@@ -50,4 +64,116 @@ export interface TransformContext extends Required<TransformOptions> {
   removeIdentifiers(exp: ExpressionNode | string): void
   hoist(exp: JSChildNode): SimpleExpressionNode
   cache<T extends JSChildNode>(exp: T, isVNode?: boolean): CacheExpression | T
+}
+
+export function createTransformContext(
+  root: RootNode,
+  {
+    prefixIdentifiers = false,
+    hoistStatic = false,
+    cacheHandlers = false,
+    nodeTransforms = [],
+    directiveTransforms = {},
+    transformHoist = null,
+    isBuiltInComponent = NOOP,
+    isCustomElement = NOOP,
+    expressionPlugins = [],
+    scopeId = null,
+    ssr = false,
+    ssrCssVars = ``,
+    bindingMetadata = {},
+    onError = defaultOnError
+  }: TransformOptions
+): TransformContext {
+  const context: TransformContext = {
+    // options
+    prefixIdentifiers,
+    hoistStatic,
+    cacheHandlers,
+    nodeTransforms,
+    directiveTransforms,
+    transformHoist,
+    isBuiltInComponent,
+    isCustomElement,
+    expressionPlugins,
+    scopeId,
+    ssr,
+    ssrCssVars,
+    bindingMetadata,
+    onError,
+
+    // state
+    root,
+    helpers: new Set(),
+    components: new Set(),
+    directives: new Set(),
+    hoists: [],
+    imports: new Set(),
+    temps: 0,
+    cached: 0,
+    identifiers: Object.create(null),
+    scopes: {
+      vFor: 0,
+      vSlot: 0,
+      vPre: 0,
+      vOnce: 0
+    },
+    parent: null,
+    currentNode: root,
+    childIndex: 0,
+
+    // methods
+    helper(name) {
+      context.helpers.add(name)
+      return name
+    },
+    helperString(name) {
+      return ``
+    },
+    replaceNode(node) {},
+    removeNode(node) {},
+    onNodeRemoved: () => {},
+    addIdentifiers(exp) {
+      // TODO
+    },
+    removeIdentifiers(exp) {
+      // TODO
+    },
+    hoist(exp) {
+      // TODO
+      return {} as any
+    },
+    cache(exp, isVNode = false) {
+      // TODO
+      return {} as any
+    }
+  }
+
+  return context
+}
+
+export function transform(root: RootNode, options: TransformOptions) {
+  // TODO
+}
+
+// TODO
+// createRootCodegen
+
+export function traverseChildren(
+  parent: ParentNode,
+  context: TransformContext
+) {
+  // TODO
+}
+
+export function traverseNode(
+  node: RootNode | TemplateChildNode,
+  context: TransformContext
+) {}
+
+export function createStructuralDirectiveTransform(
+  name: string | RegExp,
+  fn: StructuralDirectiveTransform
+): NodeTransform {
+  return {} as any
 }
