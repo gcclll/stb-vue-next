35248ceece1aa8650b65f7f7ce13612660a65397
gcclll <gccll.love@gmail.com>
2020-11-30 11:48:16 +0800

    feat(add): compiler-core exports

--- packages/compiler-core/src/index.ts
+++ packages/compiler-core/src/index.ts
@@ -1,1 +1,27 @@
+export { baseCompile } from './compile'
 export { baseParse, TextModes } from './parse'
+export {
+  transform,
+  TransformContext,
+  createTransformContext,
+  traverseNode,
+  createStructuralDirectiveTransform,
+  NodeTransform,
+  StructuralDirectiveTransform,
+  DirectiveTransform
+} from './transform'
+export { generate, CodegenContext, CodegenResult } from './codegen'
+export {
+  ErrorCodes,
+  CoreCompilerError,
+  CompilerError,
+  createCompilerError
+} from './errors'
+
+export * from './ast'
+export * from './utils'
+export * from './runtimeHelpers'
+
+export { getBaseTransformPreset, TransformPreset } from './compile'
+
+export { generateCodeFrame } from '@vue/shared'
