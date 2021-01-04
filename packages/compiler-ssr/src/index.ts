05db578e37b1bb8651d18fb7b76abb2a064235dc
gcclll <gccll.love@gmail.com>
2021-01-04 20:24:15 +0800

    feat(init): compiler-ssr -> compile function

--- packages/compiler-ssr/src/index.ts
+++ packages/compiler-ssr/src/index.ts
@@ -1,14 +1,52 @@
-export {
+import {
   CodegenResult,
   baseParse,
   parserOptions,
-  transform,
-  generate,
   CompilerOptions,
-  transformExpression,
-  trackVForSlotScopes,
-  trackSlotScopes,
-  noopDirectiveTransform,
+  transform,
   transformBind,
-  transformStyle
+  generate
 } from '@vue/compiler-dom'
+
+export function compile(
+  template: string,
+  options: CompilerOptions = {}
+): CodegenResult {
+  options = {
+    ...options,
+    // 引用 DOM parser 选项
+    ...parserOptions,
+    ssr: true,
+    scopeId: options.mode === 'function' ? null : options.scopeId,
+    // 总是加上前缀，ssr 不需要关系大小
+    prefixIdentifiers: true,
+    // ssr 下不需要缓存和静态提升优化
+    cacheHandlers: false,
+    hoistStatic: false
+  }
+
+  const ast = baseParse(template, options)
+  // TODO Save raw options for AST. This is needed when performing sub-transforms
+  // on slot vnode branches.
+
+  transform(ast, {
+    ...options,
+    nodeTransforms: [
+      // TODO ... ssr transforms
+
+      ...(options.nodeTransforms || []) // user transforms
+    ],
+    directiveTransforms: {
+      // 复用 compiler-core 的 v-bind
+      bind: transformBind,
+      // TODO ... more ssr directive transforms
+      ...(options.directiveTransforms || {}) // user transforms
+    }
+  })
+
+  // TODO traverse the template AST and convert into SSR codegen AST
+  // by replacing ast.codegenNode.
+  // 将 compiler-core 阶段生成的 codegenNode 转换成 SSR codegen AST
+
+  return generate(ast, options)
+}
