cb4474ae9c0c040bfed54178a63a3e65c16894b9
gcclll <gccll.love@gmail.com>
2021-01-26 11:07:45 +0800

    feat(add): async component

--- packages/runtime-core/src/apiAsyncComponent.ts
+++ packages/runtime-core/src/apiAsyncComponent.ts
@@ -1,4 +1,17 @@
-import { Component } from './component'
+import { isFunction, isObject } from '@vue/shared'
+import { ref } from '@vue/reactivity'
+import { defineComponent } from './apiDefineComponent'
+import {
+  Component,
+  ComponentInternalInstance,
+  ComponentOptions,
+  ConcreteComponent,
+  currentInstance
+} from './component'
+import { ComponentPublicInstance } from './componentPublicInstance'
+import { ErrorCodes, handleError } from './errorHandling'
+import { createVNode, VNode } from './vnode'
+import { warn } from './warning'
 
 export type AsyncComponentResolveResult<T = Component> = T | { default: T } // es modules
 
@@ -19,4 +32,18 @@ export interface AsyncComponentOptions<T = any> {
     fail: () => void,
     attempts: number
   ) => any
+}
+
+export const isAsyncWrapper = (i: ComponentInternalInstance | VNode) =>
+  !!(i.type as ComponentOptions).__asyncLoader
+
+export function defineAsyncComponent<
+  T extends Component = { new (): ComponentPublicInstance }
+>(source: AsyncComponentLoader<T> | AsyncComponentOptions<T>): T {
+  // 1. TODO retry 封装
+  //
+  // 2. TODO 函数封装
+  //
+  // 3. TODO 返回组件
+  return defineComponent({} as any) as any
 }
