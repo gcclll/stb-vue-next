411d0754bd1ea23e1c4f9d2441551098e14d7654
gcclll <gccll.love@gmail.com>
2021-04-06 16:30:36 +0800

    feat(add): keep-alive setup initialization

--- packages/runtime-core/src/components/KeepAlive.ts
+++ packages/runtime-core/src/components/KeepAlive.ts
@@ -1,6 +1,38 @@
-import { ConcreteComponent, SetupContext } from '../component'
-import { RendererElement, RendererInternals, RendererNode } from '../renderer'
-import { VNode, VNodeProps } from '../vnode'
+import {
+  ConcreteComponent,
+  getCurrentInstance,
+  SetupContext,
+  ComponentInternalInstance,
+  LifecycleHooks,
+  currentInstance,
+  getComponentName
+} from '../component'
+import { VNode, cloneVNode, isVNode, VNodeProps } from '../vnode'
+import { warn } from '../warning'
+import {
+  onBeforeUnmount,
+  injectHook,
+  onUnmounted,
+  onMounted,
+  onUpdated
+} from '../apiLifecycle'
+import {
+  isString,
+  isArray,
+  ShapeFlags,
+  remove,
+  invokeArrayFns
+} from '@vue/shared'
+import { watch } from '../apiWatch'
+import {
+  RendererInternals,
+  queuePostRenderEffect,
+  MoveType,
+  RendererElement,
+  RendererNode,
+  invokeVNodeHook
+} from '../renderer'
+import { setTransitionHooks } from './BaseTransition'
 import { ComponentRenderContext } from '../componentPublicInstance'
 
 type MatchPattern = string | RegExp | string[] | RegExp[]
@@ -43,7 +75,45 @@ const KeepAliveImpl = {
     max: [String, Number]
   },
 
-  setup(props: KeepAliveProps, { slots }: SetupContext) {}
+  setup(props: KeepAliveProps, { slots }: SetupContext) {
+    const cache: Cache = new Map()
+    const keys: Keys = new Set()
+    let current: VNode | null = null
+
+    const instance = getCurrentInstance()!
+    const parentSuspense = instance.suspense
+
+    const sharedContext = instance.ctx as KeepAliveContext
+    const {
+      renderer: {
+        p: patch,
+        m: move,
+        um: _unmount,
+        o: { createElement }
+      }
+    } = sharedContext
+    const storageContainer = createElement('div')
+
+    sharedContext.activate = (vnode, container, anchor, isSVG, optimized) => {}
+
+    sharedContext.deactivate = (vnode: VNode) => {}
+
+    // 对 renderer unmount 的一次封装
+    function unmount(vnode: VNode) {}
+
+    // 过滤掉缓存
+    function pruneCache(filter?: (name: string) => boolean) {}
+
+    function pruneCacheEntry(key: CacheKey) {}
+
+    // TODO 监听 include/exclude 属性变化
+    // TODO 在 render 之后缓存子树(subTree)
+    // TODO 注册生命周期
+
+    return () => {
+      // 该函数解析出原始 VNode 节点返回
+    }
+  }
 }
 
 export const KeepAlive = (KeepAliveImpl as any) as {
