38bed2c1b2ad9e7013ecabc5fbcb31e412d4de51
gcclll <gccll.love@gmail.com>
2021-04-14 18:55:14 +0800

    feat(init): runtime-dom

---
+++ packages/runtime-dom/src/index.ts
@@ -0,0 +1,24 @@
+import {
+  createRenderer,
+  createHydrationRenderer,
+  warn,
+  RootRenderFunction,
+  CreateAppFunction,
+  Renderer,
+  HydrationRenderer,
+  App,
+  RootHydrateFunction
+} from '@vue/runtime-core'
+// Importing from the compiler, will be tree-shaken in prod
+import { isFunction, isString, isHTMLTag, isSVGTag, extend } from '@vue/shared'
+
+declare module '@vue/reactivity' {
+  export interface RefUnwrapBailTypes {
+    // Note: if updating this, also update `types/refBail.d.ts`.
+    runtimeDOMBailTypes: Node | Window
+  }
+}
+
+// re-export everything from core
+// h, Component, reactivity API, nextTick, flags & types
+export * from '@vue/runtime-core'
